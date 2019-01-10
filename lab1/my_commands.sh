c93a6252454f17f6b28f2ef6f1cd6a870ae4648e
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-01-10 10:12:55 -0500

    update BASH commands

--- lab1/my_commands.sh
+++ lab1/my_commands.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-gcc -Wall -std=gnu99 -g echo_arg.c
-./a.out csc209 > echo_out.txt
-gcc -Wall -std=gnu99 -g echo_stdin.c
-./a.out < echo_stdin.c
-gcc -Wall -std=gnu99 -g count.c
-./a.out 210 | wc -m
-gcc -Wall -std=gnu99 -g echo_stdin.c
-ls -l -S | head -2 | tail -1 | cut -d " " -f 9 | ./a.out
+./echo_arg csc209 > echo_out.txt
+./echo_stdin < echo_stdin.c
+./count 210 | wc -m
+ls -l -S | head -2 | tail -1 | cut -d " " -f 9 | ./echo_stdin
