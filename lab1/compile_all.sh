bbf5a7be519e1ab67be6a39884d2b6fb585b9ce4
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-01-10 10:09:37 -0500

    modify compile_all.sh to remove unwanted commands

--- lab1/compile_all.sh
+++ lab1/compile_all.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 gcc -Wall -std=gnu99 -g -o hello hello.c
-./hello
 gcc -Wall -std=gnu99 -g -o echo_arg echo_arg.c
-./echo_arg Something
 gcc -Wall -std=gnu99 -g -o echo_stdin echo_stdin.c
-./echo_stdin
 gcc -Wall -std=gnu99 -g -o count count.c
-./count 10
