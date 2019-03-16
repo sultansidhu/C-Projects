e7108986322a0c012dbcad107928776ce62e1e1e
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-03-16 01:50:53 -0400

    psort modifications made

--- a3/psort.c
+++ a3/psort.c
@@ -24,6 +24,30 @@ void delegate_work(int work[], int num_processes, int num_entries){
   }
 }
 
+void Pipe(int * fd){
+  if ((pipe(fd))==-1){
+    perror("pipe");
+    exit(1);
+  }
+}
+
+int Fork(){
+  int result = fork();
+  if (result < 0){
+    perror("fork");
+    exit(1);
+  }
+  return result;
+}
+
+void set_file_pt_position(FILE *fp, int position, int *work_array){
+  int sum = 0;
+  for (int i = 0; i < position; i++){
+    sum += work_array[i];
+  }
+  fseek(fp, sum * sizeof(struct rec), SEEK_SET);
+}
+
 int main(int argc, char* argv[]){
   //check input
   if (argc != 7){
@@ -52,10 +76,73 @@ int main(int argc, char* argv[]){
 
   // decide how much work to delegate to each process
 
-  int * work = malloc(sizeof(int) * num_processes);
+  int * work = malloc(sizeof(int) * num_processes); // how much work per child
+  struct rec * unsorted_records = malloc(filesize); // the array of unsorted records
+  struct rec * sorted_records = malloc(filesize); // array of the sorted records
+
   printf("FREE THIS\n");
   delegate_work(work, num_processes, num_entries);
 
-  
+  // fill up the array of records in heap memory
+
+  FILE * fp = fopen(filename, "rb");
+  struct rec temporary_record;
+  for (int j = 0; j < num_entries; j++){
+    fread(&temporary_record, sizeof(struct rec), 1, fp);
+    unsorted_records[j] = temporary_record;
+  }
+  printf("CHECK IF THIS CODE BLOCK IS NEEDED.\n");
+
+  // create the pipes that will be used for communication
+
+  int fd[num_processes-1][2]; // a pipe per child process
+
+  // The master for loop begins
+
+  for (int i = 0; i < num_processes; i++){
+
+    // convert file descriptor subarray to pipe
+
+    Pipe(fd[i]);
+
+    // call fork to obtain a child process
+
+    int result;
+    if ((result = Fork()) == 0){
+
+      // read data
+
+      set_file_pt_position(fp, i, work);
+      printf("this function doesnt work^, use gdb")
+      struct rec * records = malloc(sizeof(struct rec) * 2);
+      printf("FREE THIS\n");
+      int num_records_inside_process = work[i];
+
+      // store in array (possibly malloc'd)
+
+      for (int k = 0; k < num_records_inside_process; k++){
+        fread(records, sizeof(struct rec), 1, fp);
+      }
+
+      // printf("in child process now, printing contents: \n");
+      // for (int m = 0; m < num_records_inside_process; m++){
+      //   printf("the entry is: %s\n", records[m].word);
+      // }
+
+      // call qsort on it
+
+      qsort(records, num_records_inside_process, sizeof(struct rec), compare_freq);
+
+      // printf("AFTER SORTING \n\n");
+      // for (int m = 0; m < num_records_inside_process; m++){
+      //   printf("the entry is: %s\n", records[m].word);
+      // }
+
+    } else {
+      // parent process
+    }
+  }
+
+
 
 }
